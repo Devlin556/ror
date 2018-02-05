0d440a0236f354431cf659c5db6305cca1a24fba
askesis <asketes@milkandcartoons.com>
2018-02-05 22:27:03 +0300

    Ex two complete

--- app/controllers/two_controller.rb
+++ app/controllers/two_controller.rb
@@ -3,7 +3,15 @@ class TwoController < ApplicationController
 
     @names = ['Петя', 'Алина', 'Вася', 'Никита', 'Оля', 'Ваня', 'Саша', 'Кирилл', 'Вася']
 
-    @users = [{lastName: 'Королев', role: 'admin'}, {lastName: 'Шевченко', role: 'user'}, {lastName: 'Иванов', role: 'user'}, {lastName: 'Петров', role: 'user'}]
+    @users = [
+      {lastName: 'Королев', role: 'admin'}, 
+      {lastName: 'Шевченко', role: 'user'}, 
+      {lastName: 'Иванов', role: 'user'}, 
+      {lastName: 'Петров', role: 'user'}
+    ]
+
+    @admins = @users.select { |user| user[:role] == 'admin' }
+    @notadmins = @users.select { |user| user[:role] == 'user' }
 
   end
 end
