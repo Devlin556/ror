671aee29e9f5a9459501c20b5462b58001fe4434
askesis <asketes@milkandcartoons.com>
2018-02-05 12:08:40 +0300

    Current progress

--- app/controllers/two_controller.rb
+++ app/controllers/two_controller.rb
@@ -3,5 +3,7 @@ class TwoController < ApplicationController
 
     @names = ['Петя', 'Алина', 'Вася', 'Никита', 'Оля', 'Ваня', 'Саша', 'Кирилл', 'Вася']
 
+    @users = [{lastName: 'Королев', role: 'admin'}, {lastName: 'Шевченко', role: 'user'}, {lastName: 'Иванов', role: 'user'}, {lastName: 'Петров', role: 'user'}]
+
   end
 end
