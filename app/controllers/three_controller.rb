e165fd9c72634b9a974f3b2da86aed966529b044
askesis <asketes@milkandcartoons.com>
2018-02-19 14:35:45 +0300

    Lab three complete

--- app/controllers/three_controller.rb
+++ app/controllers/three_controller.rb
@@ -1,7 +1,31 @@
 class ThreeController < ApplicationController
   def index
 
-    @password = ''
+    @password = 'pas';
 
+    @path = 'D:\Webservers\home\test\www\myfiles.txt';
+
+    @stringWithWhitespace = '   string with    whitespace   ';
+
+    @smiles = 'sdsad:(sdfsd;((';
+
+    @five = 'C Днем Рождения';
+
+    @six = 'html, <b>php</b>, js'
+
+    len = Random.new.rand(25)   
+
+    alphabet = ('a'..'z').to_a;
+    @password = '';
+
+    (1 .. Random.new.rand(25) ).map {
+      @password += alphabet[Random.new.rand(26)]
+    } 
+    
+    require 'digest/md5';
+    @md5Pass = Digest::MD5.hexdigest(@password)
+
+      # @password =+ alphabet[Randow.new.rand(26)]
+    # end 
   end
 end
