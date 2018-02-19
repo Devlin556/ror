1cb51bb88245b129b55fb3c48575cc7c924a5ce0
Alexey Maligon <fania556@gmail.com>
2018-02-19 16:30:46 +0300

    Add table generation function

--- app/controllers/four_controller.rb
+++ app/controllers/four_controller.rb
@@ -17,11 +17,29 @@ class FourController < ApplicationController
   end
 
   def getTable(cols, rows, color)
-    html_table = ''
-    (0..cols-1).each()
+    html_table = '<table class="table table-stripped table-bordered"><head><body></table>'
+    table_head = '<tr><row></tr>'
+    table_body = ''
+    header_row = ''
+    body_row = "<tr style='background: #{color}'><td></tr>"
+    body_data = ''
+    (1..cols).each { |col|
+      header_row += "<th>#{col.to_s()}</th>"
+      (1..rows ).each{ |row|
+        body_data += "<td>#{(col * row).to_s()}</td>"
+      }
+      body_row.gsub!(/<td>/, body_data)
+      body_data = ''
+      table_body += body_row
+      body_row = "<tr style='background: #{color}'><td></tr>"
+    }
+    table_head.gsub!(/<row>/, header_row)
+    html_table.gsub!(/<head>/, table_head)
+    html_table.gsub!(/<body>/, table_body)
+    return html_table.html_safe
   end
   def index
 
   end
-  helper_method :sum, :eval
+  helper_method :sum, :eval, :getTable
 end
