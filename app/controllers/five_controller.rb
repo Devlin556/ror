ab1aab1c7a666b94fd195ab5fd397ea9faa01276
askesis <asketes@milkandcartoons.com>
2018-03-18 23:57:09 +0300

    Create not working very simple form

--- app/controllers/five_controller.rb
+++ app/controllers/five_controller.rb
@@ -1,4 +1,10 @@
 class FiveController < ApplicationController
   def index
   end
+
+  def perform
+    puts params[:text]
+    redirect_to :back
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   get 'test/test'
   root 'test#test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'five/index/search' => 'five/index#perform'
+
 end
