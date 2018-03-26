1f3d378a3dfcce3c3ac7fa02efad8a9a4cd111cc
askesis <asketes@milkandcartoons.com>
2018-03-26 11:23:01 +0300

    Create model "form", required that

--- app/controllers/five_controller.rb
+++ app/controllers/five_controller.rb
@@ -1,12 +1,36 @@
 class FiveController < ApplicationController
+
   def index
+    # @@form = Form.all
+    form = Form.select(:input).distinct
+    puts form
   end
 
   def perform
-    puts params[:text]
+
+    form = Form.new
+
+    form.input = params[:input_element]
+
+    form.save
+
     # redirect_to :back
+    # puts @input_element
+
+    # @input_element = params[:input_element]
+
+    # puts @input_element
+
     redirect_back fallback_location: root_path
+
+
+    # @textarea_element = params[:textarea_element]
+    # @age = params[:age] || 0
+    # @pet_dog = params[:pet_dog] || 0
+    # @pet_cat = params[:pet_cat] || 0
 
   end
 
+
 end
+
