0e84f3dd5645bc8613581178ac82d878bf6006d0
Alexey Maligon <fania556@gmail.com>
2018-03-26 13:49:22 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post 'five/search' => 'five#perform'
+  
+  post 'login/login' => 'five#login'
 
 end
