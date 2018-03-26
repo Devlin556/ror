6bcef7f5d50b43b7674b9c87e150f6602b59e558
askesis <asketes@milkandcartoons.com>
2018-03-26 14:32:20 +0300

    Create login page

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,27 @@
 class LoginController < ApplicationController
+
+  @@users = [
+    { email: 'user1@example.com', plainpassword: 'user1@example.com' },
+    { email: 'user2@example.com', plainpassword: 'user2@example.com' },
+    { email: 'user3@example.com', plainpassword: 'user3@example.com' },
+  ]
+
   def index
+
+    email = params[:email]
+    password = params[:plainpassword]
+
+    @@users.map do |user| 
+      
+      if password == user[:plainpassword] and email == user[:email] 
+        # puts 'confirmed!!'
+        redirect_to test_test_url
+      end
+
+    end
+
+  end
+
+  def login
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,16 @@ Rails.application.routes.draw do
   get 'three/index'
   get 'four/index'
   get 'two/index'
-  
   get 'one/index'
   get 'test/test'
-  root 'five#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'login/index'
 
   post 'five/search' => 'five#perform'
   
-  post 'login/login' => 'five#login'
+  post 'login/login' => 'login#login'
+  post 'login/index' => 'login#index'
+
+  root 'five#index'
 
 end
